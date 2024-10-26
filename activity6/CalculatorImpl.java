1505a97c9dd4b7aaad305aca317974513a9ba58d
Pedro ROCHA <pedrohenriquedornele@gmail.com>
2024-10-26 16:08:00 -0300

    Update CalculatorImpl.java

--- activity6/CalculatorImpl.java
+++ activity6/CalculatorImpl.java
@@ -6,7 +6,7 @@ public class CalculatorImpl extends UnicastRemoteObject implements Calculator {
 
     // Construtor que lança 'RemoteException' e chama o construtor da superclasse.
     public CalculatorImpl() throws RemoteException {
-        super();  // Chama o construtor da classe 'UnicastRemoteObject'.
+        super(1100);  // Chama o construtor da classe 'UnicastRemoteObject'.
     }
 
     // Implementação do método remoto 'soma' que realiza a adição de dois números inteiros.
